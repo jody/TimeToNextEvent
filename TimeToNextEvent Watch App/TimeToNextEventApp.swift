53160c4789a36bb41ccf56128386111c6dcd3d88
Jody <jody@acm.org>
2025-08-30 14:12:31 -0600

    Initial Commit

---
+++ TimeToNextEvent Watch App/TimeToNextEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToNextEventApp.swift
+//  TimeToNextEvent Watch App
+//
+//  Created by Jody Paul on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeToNextEvent_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeToNextEvent Watch AppTests/TimeToNextEvent_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToNextEvent_Watch_AppTests.swift
+//  TimeToNextEvent Watch AppTests
+//
+//  Created by Jody Paul on 8/30/25.
+//
+
+import Testing
+@testable import TimeToNextEvent_Watch_App
+
+struct TimeToNextEvent_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
